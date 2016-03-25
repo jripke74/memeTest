571e7a60dc2e5fe11619b828a81fa4e4d4c1e733
Jeff Ripke <jripke74@gmail.com>
2016-03-25 06:19:00 -0700

    Fixing keyboard issues...

--- TestMeme/ViewController.swift
+++ TestMeme/ViewController.swift
@@ -104,5 +104,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     func unsubscribeFromKeyboardNotifications() {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
     }
 }
